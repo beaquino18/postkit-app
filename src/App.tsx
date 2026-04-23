89518eeb00e91a837160e393fc6eb4aeea6c33b5
Bernadette Aquino <bernadette.p.aquino@gmail.com>
2026-04-24 07:58:35 +0800

    removed strict mode, and added fixtures with main integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import './App.css'
 import SearchBar from './components/SearchBar'
 import PostList from './components/PostList'
+import { useEffect } from 'react'
+import { useStore } from './lib/store'
+import { fixtures } from './lib/fixtures'
 
 function App() {
+  const posts = useStore((state) => state.posts)
+  const addPost = useStore((state) => state.addPost)
+
+  useEffect(() => {
+    if (posts.length === 0) {
+      fixtures.forEach((post) => addPost(post))
+    }
+  }, []) 
 
   return (
     <div>
